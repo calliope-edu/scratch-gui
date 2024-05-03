45ff5b48ed1cef934db21f443fd361b3c92ff912
Haithem Bel Haj <haithem@konek.to>
2024-05-03 09:51:03 +0200

    feat(sprites): change cat movement

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -2,8 +2,8 @@ import projectData from './project-data';
 
 /* eslint-disable import/no-unresolved */
 import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
-import costume1 from '!raw-loader!./2a9fee6a9b1f71c86564321a56488b37.svg';
-import costume2 from '!raw-loader!./5ce59286cfbeb990c420416fb9cb799c.svg';
+import costume1 from '!raw-loader!./af5b4ee04f18feedd3729336a0640ef4.svg';
+import costume2 from '!raw-loader!./2a9fee6a9b1f71c86564321a56488b37.svg';
 
 /* eslint-enable import/no-unresolved */
 
@@ -32,13 +32,13 @@ const defaultProject = translator => {
             data: encoder.encode(backdrop)
         },
         {
-            id: '2a9fee6a9b1f71c86564321a56488b37',
+            id: 'af5b4ee04f18feedd3729336a0640ef4',
             assetType: 'ImageVector',
             dataFormat: 'SVG',
             data: encoder.encode(costume1)
         },
         {
-            id: '5ce59286cfbeb990c420416fb9cb799c',
+            id: '2a9fee6a9b1f71c86564321a56488b37',
             assetType: 'ImageVector',
             dataFormat: 'SVG',
             data: encoder.encode(costume2)
